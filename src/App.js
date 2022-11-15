657d574a054b8e80b1d9b88c20c6aac3490abf47
srrobin <srrobin07@gmail.com>
2022-11-16 01:34:05 +0600

    grid image gallery added by robin

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Destinations from "./components/Destinations";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
@@ -7,6 +8,7 @@ const App = () => {
     <div>
       <Navbar />
       <HeroSection />
+      <Destinations />
     </div>
   );
 };
---
+++ src/components/Destinations.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import borabora from "../assets/borabora.jpg";
+import borabora2 from "../assets/borabora2.jpg";
+import keywest from "../assets/keywest.jpg";
+import maldives from "../assets/maldives.jpg";
+import maldives3 from "../assets/maldives3.jpg";
+const Destinations = () => {
+  return (
+    <div className="max-w-[1240px] mx-auto my-16 px-4 text-center">
+      <h1>all inclusive resort</h1>
+      <p>Lorem ipsum dolor sit amet consectetur.</p>
+      <div className="grid grid-rows-none md:grid-cols-5 py-4 gap-2 md:gap-4">
+        <img
+          className="w-full h-full object-cover  col-span-2 md:col-span-3 row-span-2"
+          src={borabora}
+          alt=""
+        />
+        <img className="w-full h-full object-cover " src={borabora2} alt="" />
+        <img className="w-full h-full object-cover " src={keywest} alt="" />
+        <img className="w-full h-full object-cover " src={maldives} alt="" />
+        <img className="w-full h-full object-cover " src={maldives3} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Destinations;
